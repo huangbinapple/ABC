273d1eea455f5ece59d192aee94592d8cadce78d
Benji <huangbinappoe@gmail.com>
2025-11-22 15:01:57 +0800

    Initial Commit

---
+++ ABC/ABCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCApp.swift
+//  ABC
+//
+//  Created by 黄斌 on 2025/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ABCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
